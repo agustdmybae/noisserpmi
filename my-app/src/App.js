54e5a79bfb9b007c322de80d3206963c8e4ccd5f
Chi Chen <stephchen55@gmail.com>
2023-10-05 17:57:19 -0400

    add home page gif

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,11 @@
 //css
 import './css/App.css';
 import './css/Header.css';
+import './css/Home.css';
 
 function App() {
   return (
-    <div className="App" backgroundImage="url(/image.png)" backgroundRepeat="repeat-y">
+    <div className="App">
       <header className="header">
         <img src={require("./images/header-logo.png")} className="header-logo" alt="Noisserpmo logo" />
         <div className="header-navigation">
@@ -15,7 +16,8 @@ function App() {
         </div>
         <p className="header-time">2023/10/5</p>
       </header>
-      <section>
+      <section className="home-page">
+        <img src={require("./images/home-bg.gif")} alt="Homepage background" />
       </section>
     </div>
   );
