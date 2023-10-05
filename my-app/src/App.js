cf9db053ca7ef793257214c3565d1c72f0d8a165
Chi Chen <stephchen55@gmail.com>
2023-10-05 16:43:49 -0400

    add css files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
+//css
+import './css/App.css';
+import './css/Header.css';
 
 function App() {
   return (
@@ -7,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save.
         </p>
         <a
           className="App-link"
