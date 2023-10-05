3cf0063b0f3db5d505af39eaf1d2c8fbe2260357
Chi Chen <stephchen55@gmail.com>
2023-10-05 17:25:59 -0400

    header draft

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,11 +6,14 @@ import './css/Header.css';
 function App() {
   return (
     <div className="App" backgroundImage="url(/image.png)" backgroundRepeat="repeat-y">
-      <header className="App-header">
+      <header className="header">
         <img src={require("./images/header-logo.png")} className="header-logo" alt="Noisserpmo logo" />
-          <p>
-            Edit <code>src/App.js</code> and save.
-          </p>
+        <div className="header-navigation">
+          <button>Home</button>
+          <button>Projects</button>
+          <button>Our Team</button>
+        </div>
+        <p className="header-time">2023/10/5</p>
       </header>
       <section>
       </section>
