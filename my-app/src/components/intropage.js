da8b6d80380e057eca57121c1d8717eb7f256667
Chi Chen <stephchen55@gmail.com>
2024-04-15 17:27:54 -0400

    remain gif bg mix problem

--- my-app/src/components/intropage.js
+++ my-app/src/components/intropage.js
@@ -2,6 +2,7 @@ import '../css/Intro.css'
 export default function Intropage() {
     return (
         <section className="intro-page">
+            <img src={require("../gif/icecream.gif")} alt="Ice Cream GIF" id='ice-gif'/>
             <img src={require("../images/home-intro.png")} alt="Introduction background" id='intro-bg'/>
         </section>  
     );
