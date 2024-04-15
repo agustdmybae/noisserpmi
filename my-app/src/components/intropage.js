a7ed64f1ea7255e6ab98ec57f8638ce396f57eb2
Chi Chen <stephchen55@gmail.com>
2024-04-15 17:37:52 -0400

    add running cat gif!

--- my-app/src/components/intropage.js
+++ my-app/src/components/intropage.js
@@ -3,6 +3,7 @@ export default function Intropage() {
     return (
         <section className="intro-page">
             <img src={require("../gif/icecream.gif")} alt="Ice Cream GIF" id='ice-gif'/>
+            <img src={require("../gif/runningCat.gif")} alt="Running Cat GIF" id='runcat-gif'/>
             <img src={require("../images/home-intro.png")} alt="Introduction background" id='intro-bg'/>
         </section>  
     );
